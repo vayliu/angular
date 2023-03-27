fa675ab8d276a512b6a44b7648f5f5a9c5374c82
Vay <vayliu0302@gmail.com>
2023-03-27 17:42:46 +0800

    表单功能

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   items: Product[] = []
-  constructor() { }
+  constructor(
+    private http:HttpClient
+  ) { }
   // 加入购物车
   addToCart(product: Product) {
     this.items.push(product)
@@ -19,5 +22,9 @@ export class CartService {
   clearCart() {
     this.items = []
     return this.items
+  }
+  // 获取商品价格
+  getShippingPrices() {
+    return this.http.get<{type:string, price:number}[]>('/assets/shipping.json')
   }
 }
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService } from '../cart.service';
 
 @Component({
@@ -10,8 +11,21 @@ export class CartComponent {
 
   items = this.cartService.gteItems()
 
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
+
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder
   ){}
+
+  onSubmit(): void {
+    // 处理数据
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 
 }
