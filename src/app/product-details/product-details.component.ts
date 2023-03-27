14813759815add226e4aaba549bb705f163c3489
Vay <vayliu0302@gmail.com>
2023-03-27 16:00:11 +0800

    添加导航

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
 
+export class ProductDetailsComponent implements OnInit {
+
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute){}
+  ngOnInit() {
+    // 从路由中获取 productId
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get("productId"))
+    // 查找与路由中提供的id相对应的产品
+    this.product = products.find(product => product.id === productIdFromRoute);
+  }
 }
